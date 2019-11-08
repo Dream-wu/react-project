139d7cedd59d68334f5296fc90a3bdbc7cb2af7e
wumj <532142910@qq.com>
2019-11-08 11:44:19 +0800

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(
-        proxy(
-            // '/login', {
-            //     target: 'http://haha/',
-            //     changeOrigin: true
-            // }
-        )
-    );
+    // app.use(
+    //     proxy(
+    //         '/login', {
+    //             target: 'http://haha/',
+    //             changeOrigin: true
+    //         }
+    //     )
+    // );
 };
